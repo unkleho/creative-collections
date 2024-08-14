d3675f71beda568e14546d619e88d381059908f8
Kaho Cheung <kaho@elefanttraks.com>
2024-08-14 20:26:54 +1000

    Update slides

--- index.tsx
+++ index.tsx
@@ -21,8 +21,10 @@ const Presentation = () => (
     theme={{
       colors: {
         // primary: 'red',
-        // secondary: 'blue',
+        secondary: '#0fa6d1',
+        // secondary: '#5f0fd1',
         tertiary: 'black',
+        quaternary: '#0fa6d1',
       },
       space: [32],
       // fontSizes: {
@@ -33,12 +35,16 @@ const Presentation = () => (
     <Slide>
       <FlexBox height="100%" flexDirection="column">
         <Heading>Creative Collections</Heading>
-        <Text>Kaho Cheung</Text>
+        <Text style={{ marginTop: '-32px' }}>Kaho Cheung</Text>
+        <Text style={{ fontSize: '32px', marginTop: '-80px' }}>
+          <strong>AgriWebb</strong>
+        </Text>
       </FlexBox>
     </Slide>
 
     <MarkdownSlide animateListItems>{`
 ### About me
+- "Creative" developer / musician
 - **Powerhouse Museum** – Senior Developer (2013-2017)
 - **SLNSW** – DX Lab Tech Lead (2017-2020)
 - Currently at **AgriWebb** – Farm management platform to support sustainable cattle and sheep grazing
@@ -46,13 +52,14 @@ const Presentation = () => (
 
     <MarkdownSlide animateListItems>{`
 ### Creative Collections
-- Projects showing what is possible with technology and collections
+- Projects from **PHM** and **SLNSW**
+- Possibilities with **technology** and **collections**
 - Ask questions!
 `}</MarkdownSlide>
 
     <MarkdownSlide animateListItems>{`
-### Powerhouse Museum Collection
-- Rebuilt in 2017
+### Powerhouse Collection Website
+- Rebuilt in 2017 (refresh again in ~2021)
 - Collection data from **EMu** is harvested in realtime into a database that is exposed via an API
 - React, GraphQL, Thumbor, MongoDB, ElasticSearch
 - <a href="https://collection.powerhouse.com.au" target="phm">collection.powerhouse.com.au</a>
@@ -67,33 +74,33 @@ const Presentation = () => (
 
     <MarkdownSlide animateListItems>{`
 # DX Lab Website
-- SLNSW experimental lab (2015-2021)
+- **SLNSW** experimental lab (2015-2021)
 - Blog posts, experiments and news.
-- Also an experiment in itself, where we used new technologies in a low risk environment.
-- https://dxlab.sl.nsw.gov.au
+- Also an **experiment** in itself, where we used new technologies in a low risk environment.
+- <a href="https://dxlab.sl.nsw.gov.au" target="dxlab">dxlab.sl.nsw.gov.au</a>
 `}</MarkdownSlide>
 
     <MarkdownSlide animateListItems>{`
 # Meridian
-- View 2D maps from the 17th century in 3D
+- View **2D maps** from the **17th century** in **3D**
 - Standard image viewers are great for 2D images
-- However some images need to be viewed in 3D to be fully appreciated.
-- https://dxlab.sl.nsw.gov.au/meridian
+- Some images need to be viewed in 3D to be fully appreciated.
+- <a href="https://dxlab.sl.nsw.gov.au/meridian" target="meridian">dxlab.sl.nsw.gov.au/meridian</a>
 `}</MarkdownSlide>
 
     <MarkdownSlide animateListItems>{`
 # Virtuoso
-- Listen to 19th century sheet music from the Library's collection 
-- Makes sheet music accessible for non-musicians. 
-- Shows the importance of 'custom' viewers for different types of collection items.
-- https://dxlab.sl.nsw.gov.au/virtuoso
+- Listen to **19th century sheet music** from the Library's collection 
+- Makes sheet music **accessible** for non-musicians
+- Shows the importance of 'custom' viewers for different types of collection items
+- <a href="https://dxlab.sl.nsw.gov.au/virtuoso" target="virtuoso">dxlab.sl.nsw.gov.au/virtuoso</a>
 `}</MarkdownSlide>
 
     <MarkdownSlide animateListItems>{`
 # Art Index
-- Part dataviz, scrollytelling and search catalogue
-- Over 18,000 artworks exhibited in Australia by Australian-based artists between 1847 – 1900
-- https://dxlab.sl.nsw.gov.au/art-index
+- Part **dataviz**, **scrollytelling** and **search catalogue**
+- Over **18,000 artworks** exhibited in Australia by Australian-based artists between 1847–1900
+- <a href="https://dxlab.sl.nsw.gov.au/art-index" target="art-index">dxlab.sl.nsw.gov.au/art-index</a>
 `}</MarkdownSlide>
 
     {/* <MarkdownSlide animateListItems>{`
@@ -107,11 +114,10 @@ const Presentation = () => (
 
     <MarkdownSlide animateListItems>{`
 # #NewSelfWales
-- Award winning digital immersive experience with thousands of portraits from the Library’s collection 
-- Includes pictures from gallery selfie booth 
-- And portraits uploaded via **Instagram** from people across NSW.
-- https://www.youtube.com/watch?v=1XKn21mjInQ
-- https://dxlab.sl.nsw.gov.au/newselfwales
+- Award winning **digital immersive experience** with thousands of portraits from the Library’s collection 
+- Includes pictures from **gallery selfie booth** 
+- And selfies from **Instagram** with **#NewSelfWales**
+- <a href="https://www.youtube.com/watch?v=1XKn21mjInQ" target="nsw-video">Video</a> / <a href="https://dxlab.sl.nsw.gov.au/newselfwales" target="nsw">Website</a>
 `}</MarkdownSlide>
 
     {/* - https://dxlab.sl.nsw.gov.au/blog/making-newselfwales/ */}
@@ -119,32 +125,33 @@ const Presentation = () => (
     <MarkdownSlide animateListItems>{`
 ### State Library of NSW Collection
 - Completely new build to replace **Primo** website
-- Huge project with SLNSW web team, contractors and **DX Lab**
-- **IIIF** image viewer, AI image tags (**AWS**, **MS** and **Google**)
-- Elastic Search
-- https://collection.sl.nsw.gov.au 
+- Huge project with **SLNSW** web team, contractors and **DX Lab**
+- **IIIF** image viewer, AI image tags
+- ElasticSearch, Next JS, AWS
+- <a href="https://collection.sl.nsw.gov.au" target="slnsw-collection">collection.sl.nsw.gov.au</a>
 `}</MarkdownSlide>
 
     {/* Search Captain cook sword */}
 
     <MarkdownSlide animateListItems>{`
 ## SLNSW – AI image tags
-- https://collection.sl.nsw.gov.au/digital/0m3WozR2J0PvB
+- **AWS**, **Microsoft** and **Google**
+- <a href="https://collection.sl.nsw.gov.au/digital/0m3WozR2J0PvB">Cricket image</a>
 `}</MarkdownSlide>
 
     <MarkdownSlide animateListItems>{`
 ## SLNSW – Hierarchy viewer
-- Blog post - https://dxlab.sl.nsw.gov.au/blog/new-collection-hierarchies
-- May Gibbs - https://collection.sl.nsw.gov.au/record/n7oVw8Zn
-- APA - https://collection.sl.nsw.gov.au/record/9ALk75kY
+- May Gibbs - <a href="https://collection.sl.nsw.gov.au/record/n7oVw8Zn" target="may-gibbs">collection.sl.nsw.gov.au/record/n7oVw8Zn</a>
+- APA - <a href="https://collection.sl.nsw.gov.au/record/9ALk75kY" target="apa">collection.sl.nsw.gov.au/record/9ALk75kY</a>
+- Blog post - <a href="https://dxlab.sl.nsw.gov.au/blog/new-collection-hierarchies" target="hierarchies">dxlab.sl.nsw.gov.au/blog/new-collection-hierarchies</a>
 `}</MarkdownSlide>
 
     <MarkdownSlide animateListItems>{`
 # Creative Collections
-- Technology can enhance collection experience
-- 3D, audio and AI
-- Data is ripe for visualisations
-- Can also bring communities together
+- Technology can **enhance** collection experience
+- 3D, data viz, audio and AI
+- Small experiments can **validate** big projects
+- Can also bring **communities** together
 `}</MarkdownSlide>
 
     {/* Data should be linked to galleries, but also experiments, projects */}
